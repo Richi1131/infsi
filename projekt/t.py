f14f335025d5eaf2b12c63d7f4790c73bd28f8b4
Richard Stengel <r_c_s@hotmail.de>
2019-09-27 18:42:14 +0200

    Temp Wand

--- projekt/t.py
+++ projekt/t.py
@@ -6,13 +6,13 @@ class MyBoard(mwm.PixelBoard):
     def on_setup(self):
         self.add_image("images/backgroundCastles.png")
         player = Player((100, 100))
+        Wall((200, 200))
 
 
 class Player(mwm.Actor):
 
     def on_setup(self):
         self.add_image("images/dog.png")
-        self.costume.is_rotatable = False
         self.move_speed = 4
         self.shot_speed = 5
         self.shot_buffer = 0
@@ -21,35 +21,51 @@ class Player(mwm.Actor):
     def on_key_pressed(self, key):
         if "a" in key:
             self.direction = -90
-            self.move(self.move_speed)
-        if "d" in key:
+            if not self.blocked:
+                self.move(self.move_speed)
+        elif "d" in key:
             self.direction = 90
-            self.move(self.move_speed)
-        if "w" in key:
+            if not self.blocked:
+                self.move(self.move_speed)
+        elif "w" in key:
             self.direction = 0
-            self.move(self.move_speed)
-        if "s" in key:
+            if not self.blocked:
+                self.move(self.move_speed)
+        elif "s" in key:
             self.direction = 180
-            self.move(self.move_speed)
+            if not self.blocked:
+                self.move(self.move_speed)
         if "left" in key:
+            self.costume.orientation = -self.direction - 90
             if self.shot_buffer < 0:
                 Bullet((self.position[0], self.position[1]), -90, self.shot_speed)
                 self.shot_buffer = self.shot_cool
-        if "right" in key:
+        elif "right" in key:
+            self.costume.orientation = -self.direction + 90
             if self.shot_buffer < 0:
                 Bullet((self.position[0], self.position[1]), 90, self.shot_speed)
                 self.shot_buffer = self.shot_cool
-        if "up" in key:
+        elif "up" in key:
+            self.costume.orientation = -self.direction
             if self.shot_buffer < 0:
                 Bullet((self.position[0], self.position[1]), 0, self.shot_speed)
                 self.shot_buffer = self.shot_cool
-        if "down" in key:
+        elif "down" in key:
+            self.costume.orientation = -self.direction + 180
             if self.shot_buffer < 0:
                 Bullet((self.position[0], self.position[1]), 180, self.shot_speed)
                 self.shot_buffer = self.shot_cool
+        else:
+            self.costume.orientation = 0
+        
 
     def act(self):
         self.cool()
+        if self.sensing_tokens(5, Wall) != [] or self.sensing_borders() != []:
+            self.blocked = True
+        else:
+            self.blocked = False
+        print(self.sensing_tokens(5, Wall), self.sensing_borders())
 
     def cool(self):
         time_2 = time.time()
@@ -72,7 +88,15 @@ class Bullet(mwm.Token):
     
     def act(self):
         self.move(self.speed)
+        
+    def not_board(self):
+        self.remove()
+        
 
+class Wall(mwm.Token):
+
+    def on_setup(self):
+        self.add_image("images/cow.png")
 
 my_board = MyBoard(1000, 600)
 my_board.show()
