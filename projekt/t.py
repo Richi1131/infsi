cbe1dd853e7593e55aa795274e75edc540ef2ec8
Richard Stengel <r_c_s@hotmail.de>
2019-09-28 16:21:29 +0200

    Wand fixed, zerstörbare Wand hinzugefügt

--- projekt/t.py
+++ projekt/t.py
@@ -5,18 +5,22 @@ import time
 class MyBoard(mwm.PixelBoard):
     def on_setup(self):
         self.add_image("images/backgroundCastles.png")
-        player = Player((100, 100))
+        self.player = Player((100, 100))
         Wall((200, 200))
         DestructibleWall((300, 300))
+        Enemy((400, 400))
 
 
 class Player(mwm.Actor):
     def on_setup(self):
         self.add_image("images/char.png")
         self.speed = 4    # character move speed
+        self.hp = 100    # character hit points
         self.shot_speed = 6    # bullet move speed
-        self.shot_buffer = 0    # time till next shot (on setup always 0)
         self.shot_cool = .5    # time between shots
+        self.damage = 50
+        self.shot_buffer = 0    # time till next shot (on setup always 0)
+        self.damage_buffer = 0    # time till able to take damage (on setup always 0)
 
     def on_key_pressed(self, key):
         # movement
@@ -51,16 +55,27 @@ class Player(mwm.Actor):
     def act(self):
         self.cool()   # reducing shot cooldown
         # collision (walls and borders)
-        if self.sensing_token(Wall, 100) is not None or self.sensing_borders() != []:
+        if self.sensing_token(Wall, 100) or self.sensing_borders() != []:
             self.move_back()
+        # collision with enemies
+        if self.sensing_token(Enemy, 1):
+            self.on_hit(self.sensing_token(Enemy, 1).damage)
 
     def on_sensing_wall(self, wall):
         # additional collision to make glitches less common (not perfect)
         self.move_back()
 
+    def on_hit(self, damage):
+        if self.damage_buffer <= 0:
+            self.hp -= damage
+            self.damage_buffer = 1
+            if self.hp <= 0:
+                self.remove()
+                return
+
     def shoot(self):
-        if self.shot_buffer < 0:
-            Bullet((self.position[0], self.position[1]), self.direction+self.costume.orientation, self.shot_speed)
+        if self.shot_buffer <= 0:
+            Bullet(self)
             self.shot_buffer = self.shot_cool
 
     def cool(self):
@@ -69,33 +84,42 @@ class Player(mwm.Actor):
             d_time = time_2 - self.time
         except AttributeError:
             d_time = 0
-        self.shot_buffer -= d_time
+
+        if self.shot_buffer > 0:
+            self.shot_buffer -= d_time
+        if self.damage_buffer > 0:
+            self.damage_buffer -= d_time
+
         self.time = time.time()
 
 
 class Bullet(mwm.Token):
-    def __init__(self, pos, direction, speed):
-        super().__init__(pos)
-        self.direction = direction
-        self.speed = speed
+    def __init__(self, master):
+        self.master = master
+        super().__init__(master.position)
+        if type(master) == Player:
+            self.direction = master.direction + master.costume.orientation
+        else:
+            self.direction = master.direction
+        self.speed = master.shot_speed
+        self.damage = master.damage
 
     def on_setup(self):
         self.add_image("images/bullet.png")
-        self.move(36)    # avoiding collision with player
-    
+
     def act(self):
         self.move()
         if not self.sensing_on_board():
             self.remove()
             return
-        if self.sensing_tokens() != [self]:     # if bullet hits
-            for obj in self.sensing_tokens():   # -> trying to call on_hit() for target(s)
-                if obj != self:                 # -> -> removing bullet
-                    try:
-                        obj.on_hit()
+        if len(self.sensing_tokens()) > 1:
+            for obj in self.sensing_tokens():
+                if not isinstance(obj, Bullet) and not isinstance(obj, type(self.master)):          # if bullet hits
+                    try:                                                   # -> trying to call on_hit() for target(s)
+                        obj.on_hit(self.damage)
                     except AttributeError:
                         pass
-            self.remove()
+                    self.remove()                                                             # -> -> removing bullet
             return
 
 
@@ -108,8 +132,21 @@ class DestructibleWall(Wall):
     def on_setup(self):
         self.add_image("images/destructible_wall.png")
 
-    def on_hit(self):
+    def on_hit(self, damage):
         self.remove()
+
+
+class Enemy(mwm.Token):
+    def on_setup(self):
+        self.add_image("images/enemy.png")
+        self.hp = 100
+        self.damage = 50
+
+    def on_hit(self, damage):
+        self.hp -= damage
+        if self.hp <= 0:
+            self.remove()
+            return
 
 
 my_board = MyBoard(1000, 600)
