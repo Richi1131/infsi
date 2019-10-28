2649d88a80917c365c08ac087bf20b0f0f5f4e89
Richard Carl Stengel <rich333@hotmail.com>
2019-10-28 17:46:42 +0100

    Verbesserungen für Leveleditor, Kontenterweiterungen

--- projekt/t.py
+++ projekt/t.py
@@ -3,12 +3,13 @@ import time
 import pygame
 import matplotlib.image as image
 import os
+import random
 
 
 class MyBoard(mwm.PixelBoard):
     def on_setup(self):
         self.add_image("images/backgroundCastles.png")
-        self.player = Player((100, 100))
+        Player((100, 100))
         Wall((0, 0))
         DestructibleWall((50, 0))
         Enemy((400, 400))
@@ -19,12 +20,11 @@ class MyBoard(mwm.PixelBoard):
             self.reset()
 
     def load_room(self):
-        for token in self.get_tokens_at_rect((pygame.Rect(0, 0, 1600, 900))):
+        for token in self.get_tokens_at_rect((pygame.Rect(0, 0, res[0], res[1]))):
             token.remove()
         for object in self.room.content:
             for pos in object.positions:
                 object.type(pos)
-        Player((0, 430))
 
     def set_room(self, room):
         self.room = room
@@ -57,13 +57,14 @@ class RoomObject:
 class Player(mwm.Actor):
     def on_setup(self):
         self.add_image("images/char.png")
-        self.speed = 4    # character move speed
+        self.speed = 5    # character move speed
         self.hp = 100    # character hit points
-        self.shot_speed = 5    # bullet move speed
+        self.shot_speed = 10    # bullet move speed
         self.shot_cool = .5    # time between shots
         self.damage = 50    # character damage per shot
         self.shot_buffer = 0    # time till next shot (on setup always 0)
         self.damage_buffer = 0    # time till able to take damage (on setup always 0)
+        self.size = (80, 80)
 
     def on_key_pressed(self, key):
         # movement
@@ -110,13 +111,16 @@ class Player(mwm.Actor):
             self.costume.orientation = 0
 
     def act(self):
-        self.cool()   # reducing shot cooldown
+        self.cool()   # processing passed time
         # collision (walls and borders)
         if self.sensing_token(Wall, 1) or self.sensing_borders() != []:
             self.move_back()
-        # collision with enemies
+        # taking damage from enemies
         if self.sensing_token(Enemy, 1):
             self.on_hit(self.sensing_token(Enemy, 1).damage)
+        # collection coins
+        if self.sensing_token(Coin, 1):
+            self.sensing_token(Coin, 1).remove()
 
     def on_sensing_wall(self, wall):
         # additional collision to make glitches less common (not perfect)
@@ -136,6 +140,7 @@ class Player(mwm.Actor):
             self.shot_buffer = self.shot_cool
 
     def cool(self):
+        global d_time
         time_2 = time.time()
         try:
             d_time = time_2 - self.time
@@ -153,14 +158,14 @@ class Player(mwm.Actor):
 class Bullet(mwm.Token):
     def __init__(self, master, direction):
         self.master = master
-        super().__init__((master.position[0]+10, master.position[1]+10))
+        super().__init__((master.position[0]+40, master.position[1]+40))
         self.direction = direction
         self.speed = master.shot_speed
         self.damage = master.damage
 
     def on_setup(self):
         self.add_image("images/bullet.png")
-        self.size = (10, 10)
+        self.size = self.size = (10, 10)
 
     def act(self):
         self.move()
@@ -182,7 +187,7 @@ class Bullet(mwm.Token):
 class Wall(mwm.Token):
     def __init__(self, pos):
         super().__init__(pos)
-        self.size = (tile_size, tile_size)
+        self.size = (tile_size[0], tile_size[1])
 
     def on_setup(self):
         self.add_image("images/wall.png")
@@ -196,11 +201,46 @@ class DestructibleWall(Wall):
         self.remove()
 
 
+class Door(mwm.Token):
+    def on_setup(self):
+        self.add_image("")
+
+
+class Coin(mwm.Token):
+    def on_setup(self):
+        self.add_image("images/coin.png")
+        self.size = (80, 80)
+        self.center_x += 10
+        self.center_y += 10
+
+
 class Enemy(mwm.Token):
     def on_setup(self):
         self.add_image("images/enemy.png")
+        self.size = (80, 80)
         self.hp = 100
         self.damage = 50
+        self.speed = 5
+        self.move_buffer = 0  # time until next movement change (on setup always 0)
+        self.turn_buffer = 0  # time until next direction change (on setup always 0)
+
+    def act(self):
+        self.random_movement()
+
+    def random_movement(self):
+        self.move_buffer -= d_time
+        self.turn_buffer -= d_time
+        if self.move_buffer <= 0:
+            self.action = random.randint(0, 9)
+            self.move_buffer = random.randint(2, 9)
+        if self.turn_buffer <= 0:
+            self.direction = random.randint(-180, 180)
+            self.turn_buffer = random.randint(2, 9)
+        if self.sensing_borders() or self.sensing_token(Wall):
+            self.move_back()
+            self.direction = random.randint(-180, 180)
+        if self.action > 0:
+            self.move()
 
     def on_hit(self, damage):
         self.hp -= damage
@@ -212,24 +252,34 @@ class Enemy(mwm.Token):
 def read_levels():
     global rooms
     rooms = list()
-    for i, file in enumerate(os.listdir("maps")):
+    for file in os.listdir("maps"):
         if file.endswith(".png"):
             rooms.append(Room())
             data = image.imread(f"maps/{file}")
             for y, line in enumerate(data):
                 for x, col in enumerate(line):
-                    for key in drawables:
-                        if [round(x, 1) for x in list(col)] == drawables[key]:
-                            rooms[i].add_object(key, [(x * tile_size, y * tile_size)])
+                    col = [round(x, 1) for x in list(col)]
+                    try:
+                        rooms[len(rooms)-1].add_object(drawables[str(col)], [(x * tile_size[0], y * tile_size[1])])
+                    except KeyError:
+                        pass
 
 
 res = (1600, 900)
-tile_size = res[0]/32
+tile_count_x = 16
+tile_count_y = 9
+tile_size = (100, 100)
+
+
+d_time = 0
 drawables = {
-    Wall: [0, 0, 0, 1],
-    DestructibleWall: [0, 0.5, 0, 1]
+    "[0.0, 0.0, 1.0, 1.0]": Player, #rgba(0,0,255,255)
+    "[0.0, 0.0, 0.0, 1.0]": Wall, #rgba(0,0,0,255)
+    "[0.0, 0.5, 0.0, 1.0]": DestructibleWall, #rgba(0,~127,0,255)
+    "[1.0, 0.8, 0.0, 1.0]": Coin, #rgba(255,~200,0,255)
+    "[1.0, 0.0, 0.0, 1.0]": Enemy #rgma(255,0,0,255)
 }
 
 read_levels()
-my_board = MyBoard(1600, 900)
+my_board = MyBoard(res[0], res[1])
 my_board.show(fullscreen=True)
